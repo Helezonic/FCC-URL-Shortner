e742cfdc64116700c7dfabfa670ebe6cb5827198
Helezonic Zone A <ahamed.shuaib10@gmail.com>
2024-06-29 05:02:12 +0530

    ------dns cross verify

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const dns = require('dns');
 const app = express();
 
+
+
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended : false}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', (req, res) => {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+
+app.post('/api/shorturl', (req,res)=>{
+  const url = req.body.url;
+  console.log(url);
+  dns.lookup(url,(err,add) =>{
+    if(err) {
+      console.error(err)
+      res.json({"error" : "Invalid URL"})
+    }
+    res.send(add)
+  })
+})
+
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
